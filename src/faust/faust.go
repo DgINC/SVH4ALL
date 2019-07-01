c767b0d2164740b7d53b5548a625c677f8db290d
Dg <dg@localhost.localdomain>
2019-07-01 03:36:39 +0300

    WIP

--- src/faust/faust.go
+++ src/faust/faust.go
@@ -2,7 +2,16 @@ package faust
 
 import "C"
 
+import (
+	"github.com/DgINC/SVH4ALL/src/faust/faust/include/dsp"
+)
+
 type faustDspFactory struct {
-	dspFactoryPtr    llvm_dsp_factory
+	ptrDspFactory    llvm_dsp_factory
 	shakeyDspFactory string
+	nameDspFactory string
+}
+
+type fausDspFactoryList {
+	
 }
