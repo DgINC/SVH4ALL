b0d0ca441ca9a56d6dc75c0766664e16ca08b50d
Dg <dg@localhost.localdomain>
2019-07-03 15:32:02 +0300

    WIP

--- src/faust/faust/include/faust/dsp/llvm-c-dsp.go
+++ src/faust/faust/include/faust/dsp/llvm-c-dsp.go
@@ -7,8 +7,8 @@ import "C"
 
 import "unsafe"
 
-type llvm_dsp_factory C.uintptr
-type llvm_dsp C.uintptr
+type llvm_dsp_factory unsafe.Pointer
+type llvm_dsp unsafe.Pointer
 
 func getLibFaustVersion() string {
 	ver := C.getCLibFaustVersion()
@@ -23,45 +23,45 @@ func getDSPMachineTarget() string {
 }
 
 func getDSPFactoryFromSHAKey(sha_key string) llvm_dsp_factory {
-	cs := C.Cstring(sha_key)
+	cs := C.CString(sha_key)
 	defer C.free(unsafe.Pointer(cs))
 	pointr * llvm_dsp_factory = C.getCDSPFactoryFromSHAKey(cs)
 	return pointr
 }
 
 func createDSPFactoryFromFile(filename string, argc int32, argv []string, target string, error_msg string, opt_level int32) llvm_dsp_factory {
-	cfilename := C.Cstring(filename)
+	cfilename := C.CString(filename)
 	cArray := C.malloc(C.size_t(len(argv)) * C.size_t(unsafe.Sizeof(uintptr(0))))
 
 	cargv := (*[1<<30 - 1]*C.char)(cArray)
 
 	for index, value := range argv {
-		cargv[index] = C.Cstring(value)
+		cargv[index] = C.CString(value)
 		defer C.free(unsafe.Pointer(value))
 	}
 	defer C.free(unsafe.Pointer(cArray), unsafe.Pointer(cargv))
 
-	ctarget := C.Cstring(target)
-	cerror_msg := C.Cstring(error_msg)
+	ctarget := C.CString(target)
+	cerror_msg := C.CString(error_msg)
 	defer C.free(unsafe.Pointer(cfilename), unsafe.Pointer(cargc), unsafe.Pointer(cstring), unsafe.Pointer(cerror_msg))
 	pointr * llvm_dsp_factory = C.createCDSPFactoryFromFile(cfilename, argc, cargv, ctarget, cerror_msg, opt_level)
 	return pointr
 }
 
 func createDSPFactoryFromString(name_app string, dsp_content string, argc int32, argv []string, target string, error_msg string, opt_level int32) llvm_dsp_factory {
-	cname_app := C.Cstring(name_app)
-	cdsp_content := C.Cstring(dsp_content)
+	cname_app := C.CString(name_app)
+	cdsp_content := C.CString(dsp_content)
 	cArray := C.malloc(C.size_t(len(argv)) * C.size_t(unsafe.Sizeof(uintptr(0))))
 
 	cargv := (*[1<<30 - 1]*C.char)(cArray)
 
 	for index, value := range argv {
-		cargv[index] = C.Cstring(value)
+		cargv[index] = C.CString(value)
 		defer C.free(unsafe.Pointer(value))
 	}
 	defer C.free(unsafe.Pointer(cArray), unsafe.Pointer(cargv))
 
-	cerror_msg := C.Cstring(error_msg)
+	cerror_msg := C.CString(error_msg)
 	defer C.free(unsafe.Pointer(cname_app), unsafe.Pointer(cargc), unsafe.Pointer(cstring), unsafe.Pointer(cerror_msg))
 	pointr * llvm_dsp_factory = C.createCDSPFactoryFromString(cname_app, cdsp_content, argc, cargv, ctarget, cerror_msg, opt_level)
 	return pointr
